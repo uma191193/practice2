d54342696c0a3be1043136fbc2d77a3610c8bc10
uma191193 <uma191193@gmail.com>
2023-08-01 09:12:45 +0530

    Initial commits

---
+++ practice/src/main/java/com/maahi/practice/BasicPracticeApplication1.java
@@ -0,0 +1,13 @@
+package com.maahi.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  BasicPracticeApplication1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicPracticeApplication.class, args);
+    }
+
+}
